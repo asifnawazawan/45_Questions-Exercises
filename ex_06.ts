adc50f68c78d375470d75d21464b1676294681d4
Asif Nawaz <160779022+asifnawazawan@users.noreply.github.com>
2024-02-27 01:58:56 +0500

    Add files via upload

---
+++ ex_06.ts
@@ -0,0 +1,13 @@
+/*
+Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+*/
+
+// const myName:  string  =  "  asifnawaz  ";
+
+
+// \t used for tab/space
+const myName: string = "\asifnawaz"
+console.log(myName);
+
+console.log(myName.trim());
